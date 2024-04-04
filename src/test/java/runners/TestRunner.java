8bd490e27b761d22b43b42f7e4cdd9db365fd743
Sonal Saini <sonalsaini29@gmail.com>
2024-04-04 20:13:12 +0200

    changes

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -24,7 +24,7 @@ public class TestRunner {
 		Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
 	    Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
 	    Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
-	    Reporter.setSystemInfo("Machine", 	"Windows 10" + "64 Bit");
+	    Reporter.setSystemInfo("Machine", 	"MACOS" + "64 Bit");
 	    Reporter.setSystemInfo("Selenium", "3.7.0");
 	    Reporter.setSystemInfo("Maven", "3.5.2");
 	    Reporter.setSystemInfo("Java Version", "1.8.0_151");
